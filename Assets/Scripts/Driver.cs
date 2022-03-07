2bf536d4fe83989cd598deb718f073f62024787c
kaizersoze90 <onurkiris05@gmail.com>
2022-03-07 15:40:15 +0300

    Added some feature

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    float steerSpeed = 0.1f;
-    float moveSpeed = 0.01f;
+    [SerializeField] float steerSpeed;
+    [SerializeField] float moveSpeed;
 
     void Start()
     {
@@ -14,7 +14,20 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(0, moveSpeed, 0);
-        transform.Rotate(0, 0, steerSpeed);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float speedAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Translate(0, speedAmount, 0);
+        transform.Rotate(0, 0, -steerAmount);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Ouch!");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Got it!");
     }
 }
