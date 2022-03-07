6012e176c2c209bc3bac2a7605d8a1bd51dd6040
kaizersoze90 <onurkiris05@gmail.com>
2022-03-07 14:25:02 +0300

    Update Driver.cs

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    float steerSpeed = 0.1f;
+    float moveSpeed = 0.1f;
+
     void Start()
     {
 
@@ -11,7 +14,7 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(0, 0.1f, 0);
-        transform.Rotate(0, 0, 1);
+        transform.Translate(0, moveSpeed, 0);
+        transform.Rotate(0, 0, steerSpeed);
     }
 }
